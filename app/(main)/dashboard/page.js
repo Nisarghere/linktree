419b6f99e52ceb6509d751516c6d94b244042e05
Nisarghere <nisarg72020@gmail.com>
2026-07-10 23:52:13 +0530

    minor changes

--- app/(main)/dashboard/page.js
+++ app/(main)/dashboard/page.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>dashboard</div>
+    <>
+    <div className='bg-slate-400 min-h-screen min-w-screen flex justify-center items-center'>
+          <div className='h-130 w-100 bg-white rounded-lg mt-30 '>
+              <div>
+                
+              </div>
+          </div>
+     </div>
+    </>
   )
 }
 
