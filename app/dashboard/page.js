32f0e945d5ec3aac572829607f4c30b0c451d47d
Nisarghere <nisarg72020@gmail.com>
2026-07-12 00:16:35 +0530

    Dashboard updated

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -86,7 +86,26 @@ const page = () => {
  
         </div>
         <div className='flex justify-center'>
-          <button className=' mt-6 bg-emerald-600 text-xl font-bold  rounded-full h-10 w-10 '> + </button>
+          <button className=' mt-6 bg-black text-white cursor-pointer hover:bg-black/80 text-xl font-bold  rounded-full h-10 w-10 '> + </button>
+        </div>
+
+        <div className='bg-zinc-100 rounded-lg w-full h-40 mt-6'>
+          <h2 className='text-center'>Your LinkTree </h2>
+           <div className="bg-zinc-400 m-5 rounded-full">
+              <div className="mx-auto w-full px-6 py-4 flex justify-between items-center">
+                <p className='bg-yellow-400 px-10 py-2 rounded-full'> Linkdin </p>
+                <p>https://www.linkedin.com/company/sarkilar</p>
+                <div className='flex gap-4'>
+
+                <button className='bg-red-400 text-white rounded-lg p-2'>Edit Link</button>
+                <button className='bg-black text-white rounded-lg p-3'>Delete link</button>
+                </div>
+
+            </div>
+
+          </div>
+           
+
         </div>
 
       </section>
